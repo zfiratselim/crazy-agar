4294c23a6f5709f1d1e15c2e77b0c9900c80b899
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-23 23:34:57 +0300

    fixed create circle func

--- index.ts
+++ index.ts
@@ -6,28 +6,28 @@ export default class CrazyAgar extends PIXI.Application {
     constructor() {
         super({
             view: <HTMLCanvasElement>document.querySelector('#canvas'),
-            transparent: true,
-            width: 600,
-            height: 320,
+            backgroundColor: 0x000000,
+            width: 800,
+            height: 520,
         });
+        this.startGame();
     }
-    createCircleSprite() {
+    createCircleSprite(x: number, y: number, size: number, color: number) {
         let p = new Graphics();
-        p.beginFill(0x0FF000);
+        p.beginFill(color);
         p.lineStyle(0);
-        p.drawCircle(10, 10, 10);
+        p.drawCircle(0, 0, size);
         p.endFill();
         const t = this.renderer.generateTexture(p);
         const sprite = new PIXI.Sprite(t);
         this.stage.addChild(sprite);
-        sprite.tint=0x004523
-        sprite.x=200;
-        sprite.y=200;
+        sprite.x = x;
+        sprite.y = y;
         return sprite
     }
 
     startGame() {
-        const s = this.createCircleSprite()
+        const s = this.createCircleSprite(100, 100, 20, 0xFFFFFF)
     }
 }
 
