8fc6fa8b4f72079c455fdfdab553054f04e4c12c
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-23 23:29:57 +0300

    added createCircleSprite func

--- index.ts
+++ index.ts
@@ -11,7 +11,24 @@ export default class CrazyAgar extends PIXI.Application {
             height: 320,
         });
     }
+    createCircleSprite() {
+        let p = new Graphics();
+        p.beginFill(0x0FF000);
+        p.lineStyle(0);
+        p.drawCircle(10, 10, 10);
+        p.endFill();
+        const t = this.renderer.generateTexture(p);
+        const sprite = new PIXI.Sprite(t);
+        this.stage.addChild(sprite);
+        sprite.tint=0x004523
+        sprite.x=200;
+        sprite.y=200;
+        return sprite
+    }
 
+    startGame() {
+        const s = this.createCircleSprite()
+    }
 }
 
 (window as any).context = new CrazyAgar();
