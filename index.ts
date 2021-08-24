0388885e40f0ecd020c388dd4be3d7e9bfae842a
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-24 09:19:25 +0300

    added createEnemie func

--- index.ts
+++ index.ts
@@ -5,17 +5,22 @@ import { Sprite } from "pixi.js";
 
 export default class CrazyAgar extends PIXI.Application {
     private myCircle: Sprite;
-    constructor() {
+    private W: number;
+    private H: number;
+    constructor(W, H) {
         super({
             view: <HTMLCanvasElement>document.querySelector('#canvas'),
             backgroundColor: 0x000000,
-            width: 1000,
-            height: 620,
+            width: W,
+            height: H,
         });
+        this.W = W;
+        this.H = H;
         this.stage.interactive = true;
         this.startGame();
     }
     createCircleSprite(x: number, y: number, size: number, color: number) {
+        console.log(x, y, size, color);
         let p = new Graphics();
         p.beginFill(color);
         p.lineStyle(0);
@@ -29,16 +34,24 @@ export default class CrazyAgar extends PIXI.Application {
         this.stage.addChild(sprite);
         return sprite
     }
-
+    createEnemie() {
+        const colors: number[] = [0x45EA9A, 0xFF5652, 0x00FF11, 0xDD2589, 0x56FE12, 0xFF0000, 0x00DEFF, 0x418563, 0xAE85BB];
+        const randomX = [50, this.W - 50][Math.floor(Math.random() * 1.9)];
+        const randomY = [50, this.H - 50][Math.floor(Math.random() * 1.9)];
+        const randomSize = Math.floor(Math.random() * (this.myCircle.width * 2 - this.myCircle.width / 3) + this.myCircle.width / 3);
+        const randomColor = colors[Math.floor(Math.random() * (colors.length - 0.1))];
+        this.createCircleSprite(randomX, randomY, randomSize, randomColor);
+    }
     followPointer() {
         this.stage.on("pointermove", e => {
             this.myCircle.position.copyFrom(e.data.global);
         })
     }
     startGame() {
-        this.myCircle = this.createCircleSprite(100, 100, 15, 0xFFFFFF);
+        this.myCircle = this.createCircleSprite(500, 310, 12, 0xFFFFFF);
         this.followPointer();
+        this.createEnemie();
     }
 }
 
-(window as any).context = new CrazyAgar();
+(window as any).context = new CrazyAgar(1000, 620);
