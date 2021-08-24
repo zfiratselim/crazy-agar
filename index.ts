921c411205c76c074b5208b9578db0af614d4751
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-24 10:17:05 +0300

    added random create enemies

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { Sprite } from "pixi.js";
 
 export default class CrazyAgar extends PIXI.Application {
     private myCircle: Sprite;
+    private enemies: Sprite[] = [];
     private W: number;
     private H: number;
     constructor(W, H) {
@@ -20,7 +21,6 @@ export default class CrazyAgar extends PIXI.Application {
         this.startGame();
     }
     createCircleSprite(x: number, y: number, size: number, color: number) {
-        console.log(x, y, size, color);
         let p = new Graphics();
         p.beginFill(color);
         p.lineStyle(0);
@@ -36,11 +36,45 @@ export default class CrazyAgar extends PIXI.Application {
     }
     createEnemie() {
         const colors: number[] = [0x45EA9A, 0xFF5652, 0x00FF11, 0xDD2589, 0x56FE12, 0xFF0000, 0x00DEFF, 0x418563, 0xAE85BB];
-        const randomX = [50, this.W - 50][Math.floor(Math.random() * 1.9)];
-        const randomY = [50, this.H - 50][Math.floor(Math.random() * 1.9)];
+
+        let randomX = Math.floor((Math.random() * (this.W + 100)) - 50);
+        let randomY = Math.floor((Math.random() * (this.H + 100)) - 50);
+        if (randomX > 0 && randomX < this.W && randomY > 0 && randomY < this.H) {
+            if (Math.floor(Math.random() * 10) % 2 == 1) {
+                if (Math.floor(Math.random() * 10) % 2 == 1) {
+                    randomX = -50
+                } else {
+                    randomX = this.W + 50;
+                }
+            } else {
+                if (Math.floor(Math.random() * 10) % 2 == 1) {
+                    randomY = -50
+                } else {
+                    randomY = this.W + 50;
+                }
+            }
+        }
+        const randomRot = Math.atan2(Math.floor(Math.random() * 200 - 100 + this.H / 2 - randomY), Math.floor(Math.random() * 400 - 200 + this.W / 2 - randomX));
         const randomSize = Math.floor(Math.random() * (this.myCircle.width * 2 - this.myCircle.width / 3) + this.myCircle.width / 3);
         const randomColor = colors[Math.floor(Math.random() * (colors.length - 0.1))];
-        this.createCircleSprite(randomX, randomY, randomSize, randomColor);
+        const enemie = this.createCircleSprite(randomX, randomY, randomSize, randomColor);
+        enemie.rotation = randomRot;
+        this.enemies.push(enemie);
+    }
+    tickerFunc() {
+        const l = 2;
+        this.ticker.add(delta => {
+            console.log(this.enemies.length)
+            this.enemies.forEach((enemie, i) => {
+                enemie.x += Math.floor(Math.cos(enemie.rotation) * l);
+                enemie.y += Math.floor(Math.sin(enemie.rotation) * l);
+                if (enemie.x > this.W + 100 || enemie.x < -100 || enemie.y > this.H + 100 || enemie.y < -100) {
+                    enemie.parent.removeChild(enemie);
+                    enemie.destroy();
+                    this.enemies.splice(i, 1);
+                }
+            })
+        })
     }
     followPointer() {
         this.stage.on("pointermove", e => {
@@ -50,7 +84,15 @@ export default class CrazyAgar extends PIXI.Application {
     startGame() {
         this.myCircle = this.createCircleSprite(500, 310, 12, 0xFFFFFF);
         this.followPointer();
-        this.createEnemie();
+        let i = 0;
+        const interval = setInterval(() => {
+            i++
+            this.createEnemie();
+            if (i == 50) {
+                clearInterval(interval);
+            }
+        }, 200)
+        this.tickerFunc();
     }
 }
 
