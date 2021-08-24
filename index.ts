881e906ada5484d29fcb4e251d237a75f6077ee5
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-24 11:45:25 +0300

    finished game

--- index.ts
+++ index.ts
@@ -6,8 +6,11 @@ import { Container, Sprite } from "pixi.js";
 export default class CrazyAgar extends PIXI.Application {
     private myCircle: Sprite;
     private enemies: Sprite[] = [];
+    private click: boolean = false;
+    private alert: PIXI.Text;
     private W: number;
     private H: number;
+    private score: number = 0;
     constructor(W, H) {
         super({
             view: <HTMLCanvasElement>document.querySelector('#canvas'),
@@ -58,11 +61,18 @@ export default class CrazyAgar extends PIXI.Application {
             }
         }
         const randomRot = Math.atan2(Math.floor(Math.random() * 200 - 100 + this.H / 2 - randomY), Math.floor(Math.random() * 400 - 200 + this.W / 2 - randomX));
-        const randomSize = Math.floor(Math.random() * this.myCircle.width/2 * .8 + this.myCircle.width/2 * .3);
+        const randomSize = Math.floor(Math.random() * this.myCircle.width / 2 * 1.5 + this.myCircle.width / 2 * .3);
         const randomColor = colors[Math.floor(Math.random() * (colors.length - 0.1))];
         const enemie = this.createCircleSprite(randomX, randomY, randomSize, randomColor, this.stage.children[0] as Container);
         enemie.rotation = randomRot;
         this.enemies.push(enemie);
+    }
+    createText(text: string, color: number = 0xFFFFFF) {
+        this.alert = new PIXI.Text(text, { fontSize: 45, fill: color });
+        this.alert.anchor.set(.5);
+        this.alert.x = this.W / 2;
+        this.alert.y = this.H / 2;
+        this.stage.addChild(this.alert);
     }
     tickerFunc() {
         const l = 2;
@@ -76,45 +86,58 @@ export default class CrazyAgar extends PIXI.Application {
                 enemie.x += Math.floor(Math.cos(enemie.rotation) * l);
                 enemie.y += Math.floor(Math.sin(enemie.rotation) * l);
                 const distance = Math.sqrt(Math.pow(enemie.x - this.myCircle.x, 2) + Math.pow(enemie.y - this.myCircle.y, 2));
-                if (distance < enemie.width + this.myCircle.width) {
+                if (distance < enemie.width / 2 + this.myCircle.width / 2) {
                     if (this.myCircle.width > enemie.width) {
                         killEnemie(enemie, i);
-                        this.myCircle.width += 1;
-                        this.myCircle.height += 1;
-                        this.myCircle.texture = (this.createTexture(this.myCircle.width, 0xFFFFFF));
+                        this.score++;
+                        this.myCircle.width += 2;
+                        this.myCircle.height += 2;
+                        this.myCircle.texture = (this.createTexture(this.myCircle.width / 2, 0xFFFFFF));
                         this.createEnemie();
                         return
                     } else {
-                        console.log("killed")
+                        this.createText("Your score:" + this.score, 0xFF0000);
+                        this.ticker.stop();
                     }
                 }
                 if (enemie.x > this.W + 100 || enemie.x < -100 || enemie.y > this.H + 100 || enemie.y < -100) {
                     killEnemie(enemie, i);
-                    this.createEnemie();
+                    if (this.myCircle.width < this.H) this.createEnemie();
+                    else {
+                        this.createText("You Win. Your score: " + this.score, 0xFF0000)
+                    }
                 }
             })
         })
     }
     followPointer() {
         this.stage.on("pointermove", e => {
-            this.myCircle.position.copyFrom(e.data.global);
+            this.myCircle && this.myCircle.position.copyFrom(e.data.global);
         })
     }
+
     startGame() {
         const enemiesCon = new Container();
         this.stage.addChild(enemiesCon);
-        this.myCircle = this.createCircleSprite(500, 310, 12, 0xFFFFFF, this.stage);
-        this.followPointer();
-        let i = 0;
-        const interval = setInterval(() => {
-            i++
-            this.createEnemie();
-            if (i == 50) {
-                clearInterval(interval);
-            }
-        }, 200)
-        this.tickerFunc();
+        this.createText("Click to play game");
+        this.stage.on("pointerdown", () => {
+            if (this.click) return;
+            this.alert.parent.removeChild(this.alert);
+            this.alert.destroy();
+            this.click = true;
+            this.myCircle = this.createCircleSprite(500, 310, 12, 0xFFFFFF, this.stage);
+            this.followPointer();
+            let i = 0;
+            const interval = setInterval(() => {
+                i++
+                this.createEnemie();
+                if (i == 50) {
+                    clearInterval(interval);
+                }
+            }, 200)
+            this.tickerFunc();
+        })
     }
 }
 
-(window as any).context = new CrazyAgar(screen.width, screen.height);
+(window as any).context = new CrazyAgar(window.innerWidth, window.innerHeight);
